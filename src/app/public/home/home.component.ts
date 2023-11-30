543ea2d79b8a0fb32665858fac5ff7006a52b7f6
rajanisar288 <raaji4194@gmail.com>
2023-11-30 20:21:12 +0500

    fix landing page ui

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -14,89 +14,22 @@ export class HomeComponent implements AfterViewInit {
   menuHideAndShow: boolean = true
   modal = false;
 
-  skills = [
-    {
-      language:'Angular',
-      experience: 'mid',
-      icon:'fa-brands fa-angular',
-      per:50
-    },
-    {
-      language:'Node js',
-      experience: 'mid',
-      icon:'fa-brands fa-node-js',
-      per:50
-    },
-    {
-      language:'JavaScript',
-      experience: 'senior',
-      icon:'fa-brands fa-js',
-      per:50
-    },
-    {
-      language:'Bootstrap',
-      experience: 'mid',
-      icon:'fa-brands fa-bootstrap',
-      per:50
-    },
-    {
-      language:'Git',
-      experience: 'mid',
-      icon:'fa-brands fa-github',
-      per:50
-    },
-    {
-      language:'Sass',
-      experience: 'mid',
-      icon:'fa-brands fa-sass',
-      per:50
-    }
-
-  ]
   
-  constructor(@Inject(DOCUMENT) document: Document,private _authService:AuthService  ){}
+  
+  constructor(@Inject(DOCUMENT) document: Document,private _authService:AuthService){
 
-  ngOnInit() {
-   let  landingPage =  document.getElementsByClassName('home-navbar')
-   let item = document.getElementsByClassName('mene-item')
-   let detailCard = document.getElementById('detailCard')
-   let socialCard = document.getElementById('socialCard')
-   let linkedin = document.getElementsByClassName('linkedin')
-   let facebook = document.getElementsByClassName('facebook')
-   let twitter = document.getElementsByClassName('twitter')
-   let instagram = document.getElementsByClassName('instagram')
-    const gsapTimeLine = gsap.timeline()
-          gsapTimeLine.from(landingPage,{
-            opacity:0,
-          }).from(item,{
-            scale:2,
-            letterSpacing:-10,
-            opacity:0,
-            
-          }).from(detailCard, {
-            y:50,
-            scale:2,
-            skew:20,
-            opacity:0,
-            duration:1
-           
-          }).from(socialCard, {
-            y:50,
-            scale:2,
-            opacity:0,
-            duration:1
-
-          })
-        }
-      
-
-  //show  Modal
-  openModal(){
-    this.modal = true
   }
-  //hide Modal
-  hideModal(){
-    this.modal = false
+
+  ngOnInit() { }
+      
+ 
+  navToggle(e:any){
+    console.log(e.target);
+    e.target.classList.toggle('navToggler-left')
+    let navbar = document.getElementById('navbar')
+    navbar?.classList.toggle('navbar-hide')
+  //  let navbar =  document.getElementById('navbar-fluid')
+   console.log(navbar);
   }
   
   //NavbarToggle
