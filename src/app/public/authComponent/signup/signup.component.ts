6d847bb818d3d5c92fa8e8beeb4913b3297e6dc4
rajanisar288 <raaji4194@gmail.com>
2024-01-11 19:38:24 +0500

    test formdata manually

--- src/app/public/authComponent/signup/signup.component.ts
+++ src/app/public/authComponent/signup/signup.component.ts
@@ -8,11 +8,11 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class SignupComponent {
   steps = 1
-  formData = {
+  resgisterform = {
     profileImage: null as File | null,
     firstName:'nis',
     lastName:'ra',
-    email:'exam@gmail.com',
+    email:'exam@222gmail.com',
     password:'565dafs',
     role:'admin',
     profession:'profes',
@@ -57,33 +57,30 @@ export class SignupComponent {
   onFileSelected(event: any): void {
     const files = event.target.files;
     if (files.length > 0) {
-      this.formData.profileImage = files[0];
+      this.resgisterform.profileImage = files[0];
     }
   }
 
   register(){
 
     const formData = new FormData();
-    // formData.append('name', this.formData.name);
-  
-    // Append each field to the FormData instance
-    formData.append('profileImage', this.formData.profileImage || ''); // Ensure it's not null
-    formData.append('firstName', this.formData.firstName);
-    formData.append('lastName', this.formData.lastName);
-    formData.append('email', this.formData.email);
-    formData.append('password', this.formData.password);
-    formData.append('role', this.formData.role);
-    formData.append('profession', this.formData.profession);
-    formData.append('desc', this.formData.desc);
-    formData.append('gender', this.formData.gender);
-    formData.append('companyName', this.formData.companyName);
-    formData.append('companyWebsite', this.formData.companyWebsite);
-    formData.append('totalEmployees', this.formData.totalEmployees);
-    formData.append('companyFoundedIn', this.formData.companyFoundedIn);
-    formData.append('city', this.formData.city);
-    formData.append('country', this.formData.country);
-    formData.append('zpCode', this.formData.zpCode);
-    formData.append('street', this.formData.street);
+    formData.append('profileImage', this.resgisterform.profileImage || ''); // Ensure it's not null
+    formData.append('firstName', this.resgisterform.firstName);
+    formData.append('lastName', this.resgisterform.lastName);
+    formData.append('email', this.resgisterform.email);
+    formData.append('password', this.resgisterform.password);
+    formData.append('role', this.resgisterform.role);
+    formData.append('profession', this.resgisterform.profession);
+    formData.append('desc', this.resgisterform.desc);
+    formData.append('gender', this.resgisterform.gender);
+    formData.append('companyName', this.resgisterform.companyName);
+    formData.append('companyWebsite', this.resgisterform.companyWebsite);
+    formData.append('totalEmployees', this.resgisterform.totalEmployees);
+    formData.append('companyFoundedIn', this.resgisterform.companyFoundedIn);
+    formData.append('city', this.resgisterform.city);
+    formData.append('country', this.resgisterform.country);
+    formData.append('zpCode', this.resgisterform.zpCode);
+    formData.append('street', this.resgisterform.street);
 
     this._authService.userRegister(formData).subscribe((res)=>{
       console.log(res)
