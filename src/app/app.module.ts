b33a184470cb11d943a05680c9a11fa9e1ef88c7
rajanisar288 <raaji4194@gmail.com>
2024-05-01 21:07:20 +0500

    added modules for manager and employee and fix file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './auth/auth.service';
+import { ManagerComponent } from './features/manager/manager.component';
+import { EmployeeComponent } from './features/employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ManagerComponent,
+    EmployeeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/features/employee/empoyee.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmpoyeeRoutingModule } from './empoyee-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmpoyeeRoutingModule
+  ]
+})
+export class EmpoyeeModule { }
