845d3aace5a64a7816f924db702e9f19c0deb050
rajanisar288 <raaji4194@gmail.com>
2024-07-26 20:08:04 +0500

    fix structure for public

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,9 +9,19 @@ const routes: Routes = [
       import('./public/home/home.module').then((m) => m.HomeModule),
   },
   {
-    path: 'home',
+    path: 'plan',
     loadChildren: () =>
-      import('./public/home/home.module').then((m) => m.HomeModule),
+      import('./public/plan/plan.module').then((m) => m.PlanModule),
+  },
+  {
+    path: 'services',
+    loadChildren: () =>
+      import('./public/services/services.module').then((m) => m.ServicesModule),
+  },
+  {
+    path: 'features',
+    loadChildren: () =>
+      import('./public/features/features.module').then((m) => m.FeaturesModule),
   },
 
   {
@@ -41,17 +51,17 @@ const routes: Routes = [
     loadChildren: () =>
       import('./public/signup/signup.module').then((m) => m.SignupModule),
   },
-  {
-    path: 'home/signup',
-    loadChildren: () =>
-      import('./public/signup/signup.module').then((m) => m.SignupModule),
-  },
+  // {
+  //   path: 'home/signup',
+  //   loadChildren: () =>
+  //     import('./public/signup/signup.module').then((m) => m.SignupModule),
+  // },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full',
   },
-  { path: '**', redirectTo: 'home' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
