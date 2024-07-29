c3e1498c938e52cb31c9ce1a17253c96a44a358f
rajanisar288 <raaji4194@gmail.com>
2024-07-29 20:55:06 +0500

    fix service ui

--- src/app/public/services/services.component.ts
+++ src/app/public/services/services.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
 export class ServicesComponent {
+  mouseX: number = 0;
+  mouseY: number = 0;
+  offset: number = 20;
+  offset2: number = -60;
+  offset3: number = 100;
+  constructor() {}
 
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+  }
 }
--- src/app/public/services/services.module.ts
+++ src/app/public/services/services.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesComponent } from './services.component';
 import { PublicModule } from 'src/app/shared/components/public/public.module';
